64a02cf66853f95e753e0b043b9f2cd6198e1289
Paul Olguin <prometheus@silentflea.com>
2015-07-10 23:10:24 -0500

    Initial Commit

---
+++ simple-todos.js
@@ -0,0 +1,11 @@
+// simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+}
